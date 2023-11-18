883a89dce64caa0b6f8f108e6eaeeb4380e44709
Sitaramireddy Thatiparthi <sitaram.thatiparthi09@gmail.com>
2023-11-19 02:20:30 +0530

    project data

---
+++ src/test/java/com/app/example/sample_repo/SampleRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.example.sample_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
